3ead946cae37a45ead8fc3d110cd59d3405248be
Kyle Fleming <radman404@users.noreply.github.com>
2016-07-01 01:15:51 +0100

    Create wifi.sh

---
+++ wifi.sh
@@ -0,0 +1,63 @@
+#!/bin/env bash
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# ./wifi.sh to connect to wireless networks               #
+# quicker for new nets and keeps network configs managed  #
+# ~~~~~~~~~~~~~~KyleFleming@gmail.com~~~~~~~~~~~~~~~~~~~~~#
+if [[ $EUID -ne 0 ]]; then
+  echo "Need moar sudo, please" 1>&2
+  exit 1
+fi
+homedir = "/home/radman"
+if pgrep "wpa_supplicant" > /dev/null
+then
+  kill -15 $(pgrep wpa_supplicant)
+fi
+touch $homedir/.wpa/quit
+IFS=!
+known_loc ()
+{
+  cd $homedir/.wpa/
+  clients=(*)
+  echo "Which AP would you like to connect to?"
+  select ap in ${clients[@]};
+  do
+      if [[ $ap == "quit" ]];
+      then
+        exit 1
+      fi
+      echo $ap
+      echo "Attempting to connect"
+      connect $ap
+      break
+  done
+} &> /dev/null
+create_psk () {
+  ssid=$1
+  psk=$2
+  wpa_passphrase $ssid $psk >$homedir/.wpa/$ssid.cfg
+  connect $homedir/.wpa/$ssid.cfg
+} &> /dev/null
+connect () {
+  wpa_supplicant -c $1 -i wlp4s0 -B
+  dhcpcd wlp4s0
+} &> /dev/null
+nonwpa () {
+  iwconfig wlp4s0 essid $1
+  dhcpcd wlp4s0
+} &> /dev/null
+if [[ $1 == "" ]];
+then
+  known_loc
+elif [[ $1 == "--new" ]];
+then
+  create_psk $2 $3
+elif [[ $1 == "--essid" ]];
+then
+  nonwpa $2
+elif [[ $1 == "--help" ]];
+then
+  printf "USAGE:\t$0 --new ESSID PSK\tMake ~/.wpa/ESSID.wpa\n"
+  printf "\t$0 --essid ESSID \tFor non wpa networks\n"
+  printf "\t$0\t\t\tSelect AP from menu of AP confs in ~/.wpa\n"
+  
+fi
